1ddfce9fe53f8d601c113391d86da2a7c32a05e0
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-19 00:39:09 +0300

    added getuser and getpost methods

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getPostById, getUserById } from '../services/PostService';
 
 const Post = () => {
-    return (
-        <div className="box p-1">
-      ddd
+  const { id } = useParams();
+  const [post, setpost] = useState({})
+  const [user, setuser] = useState({})
+
+  useEffect(() => {
+    getPostById(id).
+      then(res => {
+        setpost(res.data)
+        getUserById(res.data.userId).then(res => setuser(res.data))
+      })
+  }, [])
+
+  return (
+    <div className="box p-1">
+      
     </div>
-    )
+  )
 }
 
 export default Post
--- src/services/PostService.js
+++ src/services/PostService.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 export const getPosts = () => {
     return axios.get('https://jsonplaceholder.typicode.com/posts')
-    
+};
+export const getPostById = (id) => {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+};
+export const getUserById = (id) => {
+    return axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
 };
