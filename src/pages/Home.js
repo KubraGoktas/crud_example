0f2fb26faca8f38f71ecb21521c7676c3e77b8b0
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-19 00:29:37 +0300

    added sweetalert

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Form, Input, Label } from "reactstrap";
+import Swal from "sweetalert2";
 import CustomModal from "../components/CustomModal";
 import { getPosts } from "../services/PostService";
 
 const Home = () => {
   const [posts, setposts] = useState([]);
   const [open, setOpen] = useState(false);
+
   useEffect(() => {
     getPosts().then((res) => setposts(res.data));
   }, []);
+
+  const handleDelete=(id)=>{
+    Swal.fire({
+      icon: 'question',
+      title: 'Kaydı silmek istediğinize emin misiniz?',
+      showCancelButton: true,
+      cancelButtonText:'Vazgeç',
+      confirmButtonText: 'Evet',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Kayıt Başarıyla Silindi!', '', 'success')
+      }
+    })
+  }
+
   return (
     <div className="box p-1">
       {posts.slice(0, 6).map((item, key) => {
@@ -44,7 +61,7 @@ const Home = () => {
               <Button color="primary" onClick={() => setOpen(true)}>
                 Düzenle
               </Button>
-              <Button color="primary">Sil</Button>
+              <Button color="primary" onClick={()=>handleDelete(item.id)}>Sil</Button>
             </div>
           </div>
         );
