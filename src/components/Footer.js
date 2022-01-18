2d1c4cd9fa7a7030116858072526076d1a4842bc
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-18 22:36:58 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import comp from "../assets/img/2018.png";
+import fc from "../assets/img/DEFAULT (2).png"
+import tw from "../assets/img/ICON TWITTER.png"
+import db from "../assets/img/DEFAULT (1).png"
+import gt from "../assets/img/DEFAULT.png"
+
+
+
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="container">
+      <div style={{ display: "flex", justifyContent: "space-between",alignItems:'center' }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            paddingBottom: "2%",
+          }}
+        >
+          <div className="thanks">Thank you for supporting us!</div>
+          <div className="lets">
+            {" "}
+            Let's get in touch on any of these platforms.
+          </div>
         </div>
-    )
-}
+        <div>
+        
+          <img src={fc} />
+          <img src={db} />
+          <img src={gt} />
+        </div>
+      </div>
 
-export default Footer
+      <hr />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <img src={comp} />
+        </div>
+        <div className="footer-posts">Posts</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
