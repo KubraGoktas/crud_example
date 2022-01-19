e09050267ee63d764b2091784702d9630162f768
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-19 20:09:11 +0300

    added update proccess

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import Swal from "sweetalert2";
 
-const CustomModal = ({open,setOpen,children}) => {
+const CustomModal = ({ open, setOpen, children }) => {
+  const handleUpdate = () => {
+    Swal.fire({
+      icon: 'question',
+      title: 'Kaydı silmek istediğinize emin misiniz?',
+      showCancelButton: true,
+      cancelButtonText: 'Vazgeç',
+      confirmButtonText: 'Evet',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Kayıt Başarıyla Silindi!', '', 'success')
+      }
+    })
+  }
   return (
-    <Modal isOpen={open} toggle={()=>setOpen(false)}>
-      <ModalHeader toggle={()=>setOpen(false)}>Düzenle</ModalHeader>
+    <Modal isOpen={open} toggle={() => setOpen(false)}>
+      <ModalHeader toggle={() => setOpen(false)}>Düzenle</ModalHeader>
       <ModalBody>
         {children}
       </ModalBody>
       <ModalFooter>
-        <Button style={{background: 'rgba(17, 205, 239, 1)',border:'none'}} onClick={function noRefCheck() {}}>
+        <Button style={{ background: 'rgba(17, 205, 239, 1)', border: 'none' }} onClick={() => handleUpdate()}>
           Güncelle
         </Button>
       </ModalFooter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,18 @@ import { getPosts } from "../services/PostService";
 const Home = () => {
   const [posts, setposts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState({title:'',body:''});
 
   useEffect(() => {
     getPosts().then((res) => setposts(res.data));
   }, []);
 
-  const handleDelete=(id)=>{
+  const handleDelete = (id) => {
     Swal.fire({
       icon: 'question',
       title: 'Kaydı silmek istediğinize emin misiniz?',
       showCancelButton: true,
-      cancelButtonText:'Vazgeç',
+      cancelButtonText: 'Vazgeç',
       confirmButtonText: 'Evet',
     }).then((result) => {
       if (result.isConfirmed) {
@@ -32,7 +33,7 @@ const Home = () => {
       {posts.slice(0, 6).map((item, key) => {
         return (
           <div
-          key={key}
+            key={key}
             style={{
               display: "flex",
               height: "16.67%",
@@ -58,10 +59,10 @@ const Home = () => {
               <Link className="btn" to={`/post/${item.id}`}>
                 Detay
               </Link>
-              <Button color="primary" onClick={() => setOpen(true)}>
+              <Button color="primary" onClick={() => { setOpen(true);setSelected(item) }}>
                 Düzenle
               </Button>
-              <Button color="primary" onClick={()=>handleDelete(item.id)}>Sil</Button>
+              <Button color="primary" onClick={() => handleDelete(item.id)}>Sil</Button>
             </div>
           </div>
         );
@@ -69,9 +70,9 @@ const Home = () => {
       <CustomModal open={open} setOpen={setOpen}>
         <Form>
           <Label>Title</Label>
-          <Input />
+          <Input defaultValue={selected.title}/>
           <Label>Body</Label>
-          <Input name="text" type="textarea" />
+          <Input defaultValue={selected.body} name="text" type="textarea" />
         </Form>
       </CustomModal>
     </div>
