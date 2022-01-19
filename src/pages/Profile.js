dfa860ca9896d6dd6e1c1598cfcc95b947c8234f
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-19 19:58:26 +0300

    created profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getUserById } from '../services/PostService';
+import map from '../assets/img/google map.png'
 
 const Profile = () => {
     const { id } = useParams();
@@ -9,11 +10,54 @@ const Profile = () => {
 
     useEffect(() => {
         getUserById(id).then(res => setuser(res.data))
-
     }, [])
+
     return (
-        <div className='box'>
-            profile
+        <div className='box p-4'>
+            <div className='row' style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div className='col' style={{ flexBasis: '50%', display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <span className='name'>{user?.name}</span>
+                        <span className='city'>{user?.address?.city}</span>
+                    </div>
+                    <div className='row'>
+                        <span className='col-lg-4 user-info'>Username</span>
+                        <span className='col-lg-6'>{user?.username}</span>
+                    </div>
+                    <div className='row'>
+                        <span className='col-lg-4 user-info'>Email</span>
+                        <span className='col-lg-6'>{user?.email}</span>
+                    </div>
+                    <div className='row'>
+                        <span className='col-lg-4 user-info'>Phone</span>
+                        <span className='col-lg-6'>{user?.phone}</span>
+                    </div>
+                    <div className='row'>
+                        <span className='col-lg-4 user-info'>Website</span>
+                        <span className='col-lg-6'>{user?.website}</span>
+                    </div>
+                    <div className='row'>
+                        <span className='col-lg-4 user-info'>Company</span>
+                        <span className='col-lg-6'>{user?.company?.name}</span>
+                    </div>
+                </div>
+                <div className='col' style={{ flexBasis: '50%', display: 'flex', justifyContent: 'flex-end' }}>
+                    <img width={400} src={map} />
+                </div>
+            </div>
+            <hr />
+            <div className='pt-2' style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className='desc' style={{ width: '80%', textAlign: 'center',height:'50px' }}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Ut ea nostrum numquam officiis quas commodi dolores nobis eaque veniam quos impedit sit,
+                        eligendi architecto cum vitae minima suscipit cupiditate iure ratione asperiores rerum atque aperiam deserunt est.
+                    </p>
+                    <p className='show-more'>Show more</p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
